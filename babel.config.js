c6be930f108e39d43825333b1d16476be54f97b3
CapDMarshal <fardilabintang476@gmail.com>
2026-01-04 20:18:06 +0700

    prod build

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = function (api) {
     'module:react-native-dotenv',
     {
       moduleName: '@env',
-      path: '.env.local',
+      path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local',
       safe: false,
       allowUndefined: true,
     },
